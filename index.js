865360e6498ca29403f8e51cb9ca2049dc07a1ab
张烨 <zhangye@MacBook-Pro-3.local>
2016-12-02 23:00:17 +0100

    Add some packages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http      = require('http');
 var server    = http.createServer(function(req, res) {
-  req.writeHead(200, {"Content-Type": "text/plain"});
-  req.end('Hello, Ye!\n');
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end('Hello, Ye!\n');
 }).listen('8080');
 
-console.log("Server running at http://localhost:8000/");
+console.log("Server running at http://localhost:8080/");
